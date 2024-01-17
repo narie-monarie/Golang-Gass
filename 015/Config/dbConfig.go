66ae9e6975c3e44b4d9c2cf64dcc5492ae802caf
Job Monari <jobmonari7@gmail.com>
2024-01-17 04:15:52 +0300

    want to add one to many

--- 015/Config/dbConfig.go
+++ 015/Config/dbConfig.go
@@ -9,27 +9,54 @@ import (
 var DB *sql.DB
 
 /*
+
+TODO
+one to many in SQLITE
+
 const create string = `
-  CREATE TABLE IF NOT EXISTS "people" (
-	"id"	INTEGER,
-	"first_name"	TEXT,
-	"last_name"	TEXT,
-	"email"	TEXT,
-	"ip_address"	TEXT,
-	PRIMARY KEY("id" AUTOINCREMENT)
+
+	  CREATE TABLE IF NOT EXISTS "people" (
+		"id"	INTEGER,
+		"first_name"	TEXT,
+		"last_name"	TEXT,
+		"email"	TEXT,
+		"ip_address"	TEXT,
+		PRIMARY KEY("id" AUTOINCREMENT)
+
 );
-  `
+
+		CREATE TABLE IF NOT EXISTS "people_products" (
+	    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	    "person_id" INTEGER,
+	    "product_id" INTEGER,
+	    FOREIGN KEY ("person_id") REFERENCES "people" ("id"),
+	    FOREIGN KEY ("product_id") REFERENCES "products" ("id")
+
+);
+
+`
+
+const create string = `
+
+	CREATE TABLE IF NOT EXISTS "products"(
+		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+		"name" TEXT,
+		"price" INTEGER,
+		"amount" INTEGER,
+		"date" TEXT,
+		"status" TEXT
+	);
+
+`
 */
 
 const create string = `
- CREATE TABLE IF NOT EXISTS "products"(
-	"id" INTEGER,
-	"name" TEXT,
-	"price" INTEGER,
-	"amount" INTEGER,
-	"date" TEXT,
-	"status" TEXT,
-	FOREIGN KEY (id) REFERENCES people ON DELETE CASCADE
+	CREATE TABLE IF NOT EXISTS "people_products" (
+	  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	  "person_id" INTEGER,
+	  "product_id" INTEGER,
+	  FOREIGN KEY ("person_id") REFERENCES "people" ("id"),
+	  FOREIGN KEY ("product_id") REFERENCES "products" ("id")
 	);
 `
 
