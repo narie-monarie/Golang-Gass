4a309622223ea9bc4020da736d0d0719ffa12bd4
Job Monari <jobmonari7@gmail.com>
2024-02-09 23:34:09 +0300

    testing golang 1.22

--- 018/main.go
+++ 018/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"slices"
+)
+
 func main() {
 	//update to golang 1.22
+
+	ma := []int{1, 3, 4, 5}
+	am := []int{1, 3, 4, 5, 6}
+
+	v := slices.Concat(ma, am)
+	fmt.Println(v)
 }
