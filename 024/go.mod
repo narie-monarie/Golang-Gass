563709b60d4a3a256f29c12b44fdefcd72410cde
Job Monari <jobmonari7@gmail.com>
2024-02-27 21:38:02 +0300

    trying out sqlize

--- 024/go.mod
+++ 024/go.mod
@@ -3,18 +3,54 @@ module narie/monarie
 go 1.22.0
 
 require (
+	github.com/gofiber/fiber/v2 v2.52.1
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/sunary/sqlize v0.0.0-20230902085622-54f6f02307d0
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/gofiber/fiber/v2 v2.52.1 // indirect
+	github.com/auxten/postgresql-parser v1.0.0 // indirect
+	github.com/certifi/gocertifi v0.0.0-20200922220541-2c3bb06c6054 // indirect
+	github.com/cockroachdb/apd v1.1.1-0.20181017181144-bced77f817b4 // indirect
+	github.com/cockroachdb/errors v1.8.2 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20190617123548-eb05cc24525f // indirect
+	github.com/cockroachdb/redact v1.0.8 // indirect
+	github.com/cockroachdb/sentry-go v0.6.1-cockroachdb.2 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/getsentry/raven-go v0.2.0 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/google/uuid v1.5.0 // indirect
-	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/lib/pq v1.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/pingcap/errors v0.11.4 // indirect
+	github.com/pingcap/log v0.0.0-20210906054005-afc726e70354 // indirect
+	github.com/pingcap/parser v0.0.0-20200623164729-3a18f1e5dceb // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rqlite/sql v0.0.0-20221103124402-8f9ff0ceb8f0 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/genproto v0.0.0-20200911024640-645f7a48b24f // indirect
+	google.golang.org/grpc v1.33.1 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
--- 024/main.go
+++ 024/main.go
@@ -9,87 +9,152 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var schema = `
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-	user_id    INTEGER PRIMARY KEY,
-    first_name VARCHAR(80)  DEFAULT '',
-    last_name  VARCHAR(80)  DEFAULT '',
-	email      VARCHAR(250) DEFAULT '',
-	password   VARCHAR(250) DEFAULT NULL
-);
-`
+// var schema = `
+// DROP TABLE IF EXISTS user;
+// CREATE TABLE user (
+// 	user_id    INTEGER PRIMARY KEY,
+//     first_name VARCHAR(80)  DEFAULT '',
+//     last_name  VARCHAR(80)  DEFAULT '',
+// 	email      VARCHAR(250) DEFAULT '',
+// 	password   VARCHAR(250) DEFAULT NULL
+// );
+// `
 
 type User struct {
-	UserId    int    `db:"user_id"`
-	FirstName string `db:"first_name"`
-	LastName  string `db:"last_name"`
+	ID        int32 `sql:"primary_key;autoincrement"`
+	FirstName string
+	LastName  string
 	Email     string
 	Password  sql.NullString
 }
 
-func main() {
-	app := fiber.New()
+var DB *sqlx.DB
+
+func Connect() error {
 	db, err := sqlx.Connect("sqlite3", "test.db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	db.MustExec(schema)
+	DB = db
+	return nil
+}
 
-	tx := db.MustBegin()
+func main() {
+	app := fiber.New()
+	if err := Connect(); err != nil {
+		log.Fatal(err)
+	}
 
-	tx.MustExec("INSERT INTO user (first_name, last_name, email, password) VALUES ($1, $2, $3,$4)", "Jason", "Moiron", "jmoiron@jmoiron.net", "kamatakamata")
-	tx.MustExec("INSERT INTO user (first_name, last_name, email, password) VALUES ($1, $2, $3,$4)", "John", "Doe", "johndoeDNE@gmail.net", "kamatakamata")
-	tx.NamedExec("INSERT INTO user (first_name, last_name, email, password) VALUES (:first_name, :last_name, :email, :password)",
-		&User{FirstName: "Jane", LastName: "Citizen", Email: "jane.citzen@example.com", Password: sql.NullString{String: "kamatakamata"}})
-	tx.Commit()
-	defer db.Close()
+	// newMigration := sqlize.NewSqlize(sqlize.WithSqlTag("sql"), sqlize.WithMigrationFolder("migrations"))
+	// _ = newMigration.FromObjects(User{})
+	// println(newMigration.StringUp())
 
-	app.Get("/users", func(c *fiber.Ctx) error {
-		users := []User{}
-		err := db.Select(&users, "SELECT * FROM user")
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"error": "failed to get users",
-			})
-		}
-		return c.Status(fiber.StatusOK).JSON(users)
+	//db.MustExec(newMigration.StringUp())
+
+	//tx := db.MustBegin()
+
+	// tx.MustExec("INSERT INTO user (first_name, last_name, email, password) VALUES ($1, $2, $3,$4)", "Jason", "Moiron", "jmoiron@jmoiron.net", "kamatakamata")
+	// tx.MustExec("INSERT INTO user (first_name, last_name, email, password) VALUES ($1, $2, $3,$4)", "John", "Doe", "johndoeDNE@gmail.net", "kamatakamata")
+	// tx.NamedExec("INSERT INTO user (first_name, last_name, email, password) VALUES (:first_name, :last_name, :email, :password)",
+	// 	&User{FirstName: "Jane", LastName: "Citizen", Email: "jane.citzen@example.com", Password: sql.NullString{String: "kamatakamata"}})
+	// tx.Commit()
+	// defer db.Close()
+
+	app.Get("/users", GetAllUsers)
+
+	app.Get("/user/:id", GetUser)
+
+	app.Post("/user", CreateUser)
+
+	app.Delete("/user/:id", DeleteUser)
+
+	app.Patch("/user/:id", UpdateUser)
+
+	app.Listen(":42069")
+}
+
+func GetUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	user := User{}
+	err := DB.Get(&user, "SELECT * FROM user WHERE user_id = ?", id)
+
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "User not found",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(user)
+}
+
+func CreateUser(c *fiber.Ctx) error {
+	user := User{}
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid Request",
+		})
+	}
+	tx := DB.MustBegin()
+	tx.MustExec(
+		"INSERT INTO user (first_name, last_name, email, password) VALUES ($1, $2, $3,$4)",
+		user.FirstName,
+		user.LastName,
+		user.Email,
+		user.Password,
+	)
+	if err := tx.Commit(); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to create new user",
+		})
+	}
+	return c.Status(fiber.StatusCreated).JSON(user)
+}
+
+func UpdateUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	user := User{}
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid Request",
+		})
+	}
+	tx := DB.MustBegin()
+	tx.MustExec(
+		"UPDATE user SET first_name = $1, last_name = $2, email = $3, password = $4 WHERE user_id = $?",
+		user.FirstName,
+		user.LastName,
+		user.Email,
+		user.Password,
+		id,
+	)
+	if err := tx.Commit(); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to update user",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(user)
+}
+
+func DeleteUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	tx := DB.MustBegin()
+	tx.MustExec("DELETE FROM user WHERE user_id = $?", id)
+	if err := tx.Commit(); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to delete user",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "user deleted",
 	})
+}
 
-	app.Get("/user/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-		user := User{}
-		err := db.Get(&user, "SELECT * FROM user WHERE user_id = ?", id)
-
-		if err != nil {
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-				"message": "User not found",
-			})
-		}
-		return c.Status(fiber.StatusOK).JSON(user)
-	})
-
-	app.Post("/user", func(c *fiber.Ctx) error {
-		user := User{}
-		if err := c.BodyParser(&user); err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"message": "Invalid Request",
-			})
-		}
-		tx := db.MustBegin()
-		tx.MustExec(
-			"INSERT INTO user (first_name, last_name, email, password) VALUES ($1, $2, $3,$4)",
-			user.FirstName,
-			user.LastName,
-			user.Email,
-			user.Password,
-		)
-		if err := tx.Commit(); err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"error": "failed to create new user",
-			})
-		}
-		return c.Status(fiber.StatusCreated).JSON(user)
-	})
-	app.Listen(":3000")
+func GetAllUsers(c *fiber.Ctx) error {
+	users := []User{}
+	err := DB.Select(&users, "SELECT * FROM user")
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to get users",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(users)
 }
